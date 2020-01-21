8722630a728358cf584d3ae5939af7bfbeb69fee
Wierdcreations <33354033+Wierdtangaming@users.noreply.github.com>
2020-01-21 20:04:28 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,27 @@
+class Box {
+    constructor(x, y, width, height) {
+      var options = {
+          'restitution':0.0,
+          'friction':10.3,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      push();
+      translate(pos.x,pos.y);
+      rotate(this.body.angle);
+      rectMode(CENTER);
+      fill(128,128,128);
+      rect(0 , 0, this.width, this.height);
+      pop();
+  
+    }
+  };
+  
+
